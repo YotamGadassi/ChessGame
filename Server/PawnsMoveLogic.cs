2b7ac398d1ca01368a1dbae5733352e70c29bf8e
Yotam <yotamgadassi1@gmail.com>
2021-10-28 09:08:38 +0300

    pawns move logic

---
+++ Server/PawnsMoveLogic.cs
@@ -0,0 +1,71 @@
+﻿using Common;
+using System.Windows;
+
+namespace Server
+{
+    public static class PawnsMoveLogic
+    {
+        public static bool IsMoveLegal(Move Move, bool MoveForward , bool IsFirstMove, bool IsKilling)
+        {
+            Vector diff = Move.Diff;
+
+            if (MoveForward)
+            {
+                return logicForMoveForward(diff, IsFirstMove, IsKilling);
+            }
+            else
+            {
+                logicForMoveBackward(diff, IsFirstMove, IsKilling);
+            }
+
+        }
+
+        private static bool logicForMoveForward(Vector diff, bool isFirstMove, bool isKilling)
+        {
+            if (diff.X == 0 && diff.Y == 1)
+            {
+                return true;
+            }
+
+            if (isKilling)
+            {
+                if(diff.X == 1 && diff.Y == 1)
+                {
+                    return true;
+                }
+            }
+
+            if (isFirstMove)
+            {
+                if(diff.X == 0 && diff.Y == 2)
+                {
+                    return true;                    
+                }
+            }
+        }
+
+        private static bool logicForMoveBackward(Vector diff, bool isFirstMove, bool isKilling)
+        {
+            if (diff.X == 0 && diff.Y == -1)
+            {
+                return true;
+            }
+
+            if (isKilling)
+            {
+                if(diff.X == -1 && diff.Y == -1)
+                {
+                    return true;
+                }
+            }
+
+            if (isFirstMove)
+            {
+                if(diff.X == 0 && diff.Y == -2)
+                {
+                    return true;                    
+                }
+            }
+        }
+    }
+}
