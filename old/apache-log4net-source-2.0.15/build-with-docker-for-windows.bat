157aa2b69fa70f84d3f1aa723f59e92a7f875653
YotamGadassi <Yotamgadassi1@gmail.com>
2022-10-17 13:42:26 +0300

    remove dlls

--- old/apache-log4net-source-2.0.15/build-with-docker-for-windows.bat
+++
@@ -1,1 +0,0 @@
-docker run -v %~dp0%:C:\dev -v %USERPROFILE%\.nuget\packages:C:\packages -t davydm/net-build-tools:vs2019 "npm ci && npm run build"
--- old/apache-log4net-source-2.0.15/local-tasks/prepare-build-artifacts.js
+++
@@ -1,8 +0,0 @@
-const 
-    gulp = requireModule("gulp");
-
-gulp.task("prepare-build-artifacts", gulp.series(
-    "zip",
-    "prefix-build-artifacts",
-    "hash-build-artifacts"
-));
