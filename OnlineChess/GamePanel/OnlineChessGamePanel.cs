f6ed76f7cd654587251e59c75f005f47990fa54c
YotamGadassi <Yotamgadassi1@gmail.com>
2023-09-22 14:34:41 +0300

    Add logic to OnlineChessGamePanel

--- OnlineChess/GamePanel/OnlineChessGamePanel.cs
+++ OnlineChess/GamePanel/OnlineChessGamePanel.cs
@@ -2,16 +2,18 @@
 using System.Windows.Controls;
 using Client.Game;
 using FrontCommon;
+using OnlineChess.Client;
 
 namespace OnlineChess.GamePanel;
 
 public class OnlineChessGamePanel : BaseGamePanel
 {
-    public override DependencyObject       GameViewModel { get; }
+    public override DependencyObject       GameViewModel => m_gameViewModel;
     public override Control                GameControl   => m_gameControl;
     public          OnlineChessGameManager GameManager   { get; set; }
 
-    private GameControl m_gameControl;
+    private GameControl          m_gameControl;
+    private OnlineChessViewModel m_gameViewModel;
 
     public OnlineChessGamePanel(string panelName) : base(panelName)
     {
@@ -20,19 +22,26 @@ public class OnlineChessGamePanel : BaseGamePanel
 
     public void SetGameManager(OnlineChessGameManager gameManager)
     {
-        GameManager = gameManager;
-        //TODO: Create View Model
+        GameManager             = gameManager;
+        m_gameViewModel         = new OnlineChessViewModel(gameManager);
+        GameControl.DataContext = null;
+        GameControl.DataContext = m_gameViewModel;
     }
 
-    public override void Init() { }
+    public override void Init()
+    {
+        //TODO: implement
+    }
 
     public override void Reset()
     {
+        //TODO: implement
         throw new NotImplementedException();
     }
 
     public override void Dispose()
     {
+        //TODO: implement
         throw new NotImplementedException();
     }
 }
