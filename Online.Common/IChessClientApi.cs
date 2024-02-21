fe2570126041e5b828011f91496248c7a485c643
YotamGadassi <Yotamgadassi1@gmail.com>
2024-02-21 21:15:52 +0200

    Create IChessClientAPI

---
+++ Online.Common/IChessClientApi.cs
@@ -0,0 +1,24 @@
+﻿using Board;
+using Common;
+using Tools;
+
+namespace OnlineChess.Common
+{
+    public interface IChessClientApi
+    {
+        public void StartGame(GameConfig gameConfig);
+
+        public void EndGame(EndGameReason endGameReason);
+
+        public void BoardCommandsHandler(BoardCommand[] boardCommands);
+
+        public Task<ITool> AskPromote(BoardPosition positionToPromote);
+
+        public void UpdateTime(TeamId   teamId
+                             , TimeSpan timeSpan);
+
+        public void UpdatePlayingTeam(TeamId teamId);
+
+        public void UpdateToolsAndTeams(ToolAndTeamPair[] pairs);
+    }
+}
