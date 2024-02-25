010e6d4d8e165b3a0987b87b792ef27b5b589ee6
Yotam <yotamgadassi1@gmail.com>
2024-02-25 14:38:26 +0200

    Add TeamId and ToolId converters to server

--- ChessServer/Program.cs
+++ ChessServer/Program.cs
@@ -1,21 +1,30 @@
-using ChessServer;
 using ChessServer.ServerManager;
 using Common;
 using Tools;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSignalR()
-       .AddJsonProtocol(option =>
-                        {
-                            option.PayloadSerializerOptions.Converters.Add(new IToolConverter());
-                        });
+namespace ChessServer;
 
-builder.Logging.AddLog4Net("loggingConfiguration.xml");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddSignalR()
+               .AddJsonProtocol(option =>
+                                {
+                                    option.PayloadSerializerOptions.Converters.Add(new IToolConverter());
+                                    option.PayloadSerializerOptions.Converters.Add(new TeamIdConverter());
+                                    option.PayloadSerializerOptions.Converters.Add(new ToolIdConverter());
+                                });
 
-builder.Services.Add(new ServiceDescriptor(typeof(IServerManager<string>), typeof(SignalRServerManager), ServiceLifetime.Singleton));
-WebApplication                       app = builder.Build();
+        builder.Logging.AddLog4Net("loggingConfiguration.xml");
 
-app.MapGet("/", () => "Hello World!");
-app.MapHub<ChessHub>("/ChessHub");
-app.Run();
+        builder.Services.Add(new ServiceDescriptor(typeof(IServerManager<string>), typeof(SignalRServerManager)
+                                                 , ServiceLifetime.Singleton));
+        WebApplication app = builder.Build();
 
+        app.MapGet("/", () => "Hello World!");
+        app.MapHub<ChessHub>("/ChessHub");
+        app.Run();
+    }
+}
